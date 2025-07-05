817eafff0a5d507128026beb331905451a641c0e
eacosta <eacosta0411@gmail.com>
2025-07-05 12:23:26 -0400

    Initialize variables for grid creation in script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Will use script to create an X by X grid
+
+
+
+// Initializing some of the variables that will be needed later
+const boxesContainer = document.querySelector('#boxes-container');
+
+const boxes = document.createElement("div");
+boxes.id = "box"; // These will be the boxes that will be in the etch a sketch
