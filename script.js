75864198c96252f816ff0117537b1a152547e930
eacosta <eacosta0411@gmail.com>
2025-07-05 15:02:42 -0400

    Fully functioning grid creation function implemented

--- script.js
+++ script.js
@@ -10,13 +10,17 @@ const boxesContainer = document.querySelector('#boxes-container');
 
 // Add a function that will be used to create the grid of boxes in the etch a sketch
 function grid(size) {
+    boxSize = (100 / size) - 1;
+    boxSize.toString();
     for (let i = 0; i < size**2; i++) {
         const boxes = document.createElement("div");
         boxes.id = "box";
+        boxes.style.width = boxSize + "%";
+        boxes.style.height = boxSize + "%";
         boxesContainer.appendChild(boxes);
     };
 }
 
 // quick little button for testing grid function
 const btn =  document.querySelector("#btn");
-btn.addEventListener ("click", () => {grid(2)})
+btn.addEventListener ("click", () => {grid(14)})
