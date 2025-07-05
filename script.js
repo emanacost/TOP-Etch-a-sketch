69e88b07f685efab6e34402db3dbe936c7ebe353
eacosta <eacosta0411@gmail.com>
2025-07-05 14:00:22 -0400

    Add logic for creating girds with for loop

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ console.log("I'm connected!")
 // Initializing some of the variables that will be needed later
 const boxesContainer = document.querySelector('#boxes-container');
 
-const boxes = document.createElement("div");
-boxes.id = "box"; // These will be the boxes that will be in the etch a sketch
+// const boxes = document.createElement("div");
+// boxes.id = "box"; // These will be the boxes that will be in the etch a sketch
 
 // Add a function that will be used to create the grid of boxes in the etch a sketch
 function grid(size) {
     for (let i = 0; i < size**2; i++) {
+        const boxes = document.createElement("div");
+        boxes.id = "box";
         boxesContainer.appendChild(boxes);
     };
 }
+
+// quick little button for testing grid function
+const btn =  document.querySelector("#btn");
+btn.addEventListener ("click", () => {grid(2)})
